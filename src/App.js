47cb41297733f95f74c31d7737ede15521af81a4
Eric Mauldin <emauldin84@gmail.com>
2019-11-20 10:16:58 -0500

    displays name and activity in Activity component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./App.css";
 
 import Activity from './components/Activity'
+import { StateProvider } from './context/index'
 
 function App() {
+
+  const initialState= {
+    name: 'Koa',
+    activity: 'dogging'
+  }
+
   return (
     <div className="App">
-      <Activity />
+      <StateProvider value={initialState}>
+        <Activity />
+      </StateProvider>
     </div>
   )
+
 }
 
 export default App;
--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import StateContext from '../context/index'
 
 const Activity = () => {
+    const value = useContext(StateContext)
+    console.log(value)
     return (
         <div>
-            <h1>Cat reducer</h1>
-            <p>The cat's activity.</p> 
+            <h1>Cat-Dog reducer</h1>
+            <p>{value.name} is {value.activity}</p> 
             <ul>
                 <li>
-                    <button onClick={''}>Eat</button>
+                    {/* <button onClick={''}>Eat</button> */}
                 </li>
                 <li>
-                    <button onClick={''}>Nap</button>
+                    {/* <button onClick={''}>Nap</button> */}
                 </li>
                 <li>
-                    <button onClick={''}>Play</button>
+                    {/* <button onClick={''}>Play</button> */}
                 </li>
             </ul>
         </div>
