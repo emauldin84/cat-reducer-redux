e1d438d7744c4374174667e5b0cee7d73104e067
Eric Mauldin <emauldin84@gmail.com>
2019-11-20 10:55:22 -0500

    allows user to change pet name by typing into input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 
 import Activity from './components/Activity'
@@ -11,9 +11,26 @@ function App() {
     activity: 'dogging'
   }
 
+  const reducer = (state, action) => {
+    switch(action.type) {
+      case 'changeActivity':
+        return {
+          ...state,
+          activity: action.activity
+        }
+        case 'changeName':
+          return {
+            ...state,
+            name: action.newName
+          }
+      default:
+        return state
+    }
+  }
+
   return (
     <div className="App">
-      <StateProvider value={initialState}>
+      <StateProvider value={useReducer(reducer, initialState)}>
         <Activity />
       </StateProvider>
     </div>
--- src/components/Activity.js
+++ src/components/Activity.js
@@ -2,21 +2,37 @@ import React, { useContext } from 'react'
 import StateContext from '../context/index'
 
 const Activity = () => {
-    const value = useContext(StateContext)
-    console.log(value)
+    const [value, dispatch] = useContext(StateContext)
+
+    const handleClick = (e) => {
+        dispatch({
+            type: 'changeActivity',
+            activity: e.target.value
+        })
+    }
+
+    const handleChange = (e) =>{
+        dispatch({
+            type:'changeName',
+            newName: e.target.value
+        })
+    }
     return (
         <div>
-            <h1>Cat-Dog reducer</h1>
-            <p>{value.name} is {value.activity}</p> 
+            <h1>The Cat-Dog activity monitor</h1>
+            <p>{value.name} is {value.activity}</p>
+            <p>
+                <input type='text' name='newName' placeholder='Name' onChange={handleChange}/>
+            </p>
             <ul>
                 <li>
-                    {/* <button onClick={''}>Eat</button> */}
+                    <button onClick={handleClick} value='eating'>Eat</button>
                 </li>
                 <li>
-                    {/* <button onClick={''}>Nap</button> */}
+                    <button onClick={handleClick} value='napping'>Nap</button>
                 </li>
                 <li>
-                    {/* <button onClick={''}>Play</button> */}
+                    <button onClick={handleClick} value='playing'>Play</button>
                 </li>
             </ul>
         </div>
