e15a0247a4a088e907cb7e6d22d4aad2840fe191
Eric Mauldin <emauldin84@gmail.com>
2019-11-20 09:47:59 -0500

    ads context folder and index.js context config

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const Activity = ({activity, catNap, catEat, catPlay}) => {
+const Activity = () => {
     return (
         <div>
             <h1>Cat reducer</h1>
-            <p>The cat is {activity}.</p> 
+            <p>The cat's activity.</p> 
             <ul>
                 <li>
-                    <button onClick={catEat}>Eat</button>
+                    <button onClick={''}>Eat</button>
                 </li>
                 <li>
-                    <button onClick={catNap}>Nap</button>
+                    <button onClick={''}>Nap</button>
                 </li>
                 <li>
-                    <button onClick={catPlay}>Play</button>
+                    <button onClick={''}>Play</button>
                 </li>
             </ul>
         </div>
